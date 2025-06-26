db7ab9905c244194c974ed81fafe78054987def8
Ahmed <Ahmedjader837@gmail.com>
2025-06-26 00:02:47 -0400

    feat: add TextEffect component for animated text rendering; update utils and package dependencies

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,9 @@
 import { customAlphabet } from "nanoid";
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export const nanoid = customAlphabet("abcdefghijklmnopqrstuvwxyz0123456789");
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
