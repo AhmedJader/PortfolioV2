fbe6dac542e63f90d875ac61dc4ca6f297fb3886
Ahmed <Ahmedjader837@gmail.com>
2025-06-28 16:00:09 -0400

    refactor: remove unused Dock component import for code cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 import { useEffect, useState } from 'react';
 import ChatWrapper from '@/components/ChatWrapper';
 import ProjectFeed from '@/components/ProjectFeed';
-import { Dock } from '@/components/motion-primitives/dock';
 
 export default function Home() {
   const [isInverted, setIsInverted] = useState(false);
